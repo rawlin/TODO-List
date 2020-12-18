ed62ca1e9cfeae82b46e974364a5f8d2ee260e60
rawlin <rawlincrasto@gmail.com>
2020-12-18 13:15:22 +0530

    Search bar and Flows with query change

--- app/src/main/java/com/example/todolist/ui/tasks/TaskViewModel.kt
+++ app/src/main/java/com/example/todolist/ui/tasks/TaskViewModel.kt
@@ -4,12 +4,19 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.todolist.data.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 
 class TaskViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ):ViewModel() {
 
-    val tasks = taskDao.getAllTask().asLiveData()
+    val searchQuery = MutableStateFlow("")
+
+    private val taskFlow = searchQuery.flatMapLatest {
+        taskDao.getAllTask(it)
+    }
+    val tasks = taskFlow.asLiveData()
 
 
 }
