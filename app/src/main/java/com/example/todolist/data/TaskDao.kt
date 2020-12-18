23755ccbf0d2f43832441da4ee36779732fc428b
rawlin <rawlincrasto@gmail.com>
2020-12-18 15:41:21 +0530

    Combining multiple Flows

--- app/src/main/java/com/example/todolist/data/TaskDao.kt
+++ app/src/main/java/com/example/todolist/data/TaskDao.kt
@@ -1,13 +1,23 @@
 package com.example.todolist.data
 
 import androidx.room.*
+import com.example.todolist.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
 
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getAllTask(searchQuery: String):Flow<List<Task>>
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when(sortOrder) {
+            SortOrder.BY_DATE -> getTaskSortedByDate(query, hideCompleted)
+            SortOrder.BY_NAME -> getTaskSortedByName(query, hideCompleted)
+        }
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getTaskSortedByName(searchQuery: String, hideCompleted: Boolean):Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getTaskSortedByDate(searchQuery: String, hideCompleted: Boolean):Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task:Task)
